984635161d1d07d0523e1a776ee25b03ad2a3c43
zhangmy <rekozxm@outlook.com>
2022-07-22 15:49:50 +0800

    feat(layout):chang router

--- frame/main.tsx
+++ frame/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter, useRoutes } from 'react-router-dom'
+import { BrowserRouter, useRoutes } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import routers from '@router'
 import '@styles'
@@ -10,9 +10,9 @@ const GetRoutes = () => useRoutes(routers)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider componentSize="small">
-      <HashRouter>
+      <BrowserRouter basename='/'>
         <GetRoutes></GetRoutes>
-      </HashRouter>
+      </BrowserRouter>
     </ConfigProvider>
   </React.StrictMode>
 )
